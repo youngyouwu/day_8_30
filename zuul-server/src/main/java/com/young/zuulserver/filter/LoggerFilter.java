1999941d593623de618d2c259065b5232a463ee9
youngyouwu <youngyouwu@163.com>
2018-09-01 11:51:05 +0800

    添加zuul模块

---
+++ zuul-server/src/main/java/com/young/zuulserver/filter/LoggerFilter.java
@@ -0,0 +1,44 @@
+package com.young.zuulserver.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 记录请求信息
+ *
+ * @author young
+ * create_date 2018/8/31 18:39
+ * @version 1.0
+ */
+@Component
+public class LoggerFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(LoggerFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
+        return null;
+    }
+}
